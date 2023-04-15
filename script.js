5354e109e5c53b705f250ddce5e16a2c72a873e7
duplatano <130814350+duplatano@users.noreply.github.com>
2023-04-14 23:27:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ function rickRoll() {
 
   var cel = document.getElementById("cel").value;
   var mail = document.getElementById("mail").value;
-  // alert(cel+", "+mail);
-  window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+  var tyc = document.getElementById("cbox1").checked;
+
+  if (cel && mail && tyc) {
+
+    window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+
+  } else {
+    alert("No hagas trampa :)");
+
+  }
+  //
 }
