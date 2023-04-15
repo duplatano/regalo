a0ee619a609ed5483bd17c72c9a3dc85cbbe03f7
duplatano <130814350+duplatano@users.noreply.github.com>
2023-04-14 22:41:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function rickRoll() {
+
+  var cel = document.getElementById("cel").value;
+  var mail = document.getElementById("mail").value;
+  // alert(cel+", "+mail);
+  window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+}
